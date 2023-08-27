161ea9d69ccdd9daf8c8cf4947239a9829800323
Amit Personal <amitbiradar648@gmail.com>
2023-08-27 09:34:05 +0530

    Added HTML code

--- backend1/backendApp.py
+++ backend1/backendApp.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "About Page"
+    return render_template('frontend.html')
 
 if __name__ == '__main__':
     app.run()
